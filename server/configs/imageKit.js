c5f2ebfd9984d2a16d9c636098ac252ddb63805b
MUKUL SHARMA <mukulsharma@MUKULs-MacBook-Air.local>
2025-08-26 22:08:35 +0530

    imagekit file

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,5 +1,5 @@
 import ImageKit from "imagekit";
-var imagekit = new ImageKit({
+const imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT,
